e801c8c34e1fb609ca0fcd87f9864de7f0669ef2
mariana-reis <mariana.reis@al.resilia.com.br>
2026-07-20 10:24:26 -0300

    feat: create application layout

---
+++ estoque-frontend/app/layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex min-h-screen bg-gray-50 dark:bg-gray-950 font-sans">
+    <Sidebar />
+
+    <div class="flex-1 flex flex-col overflow-hidden">
+      <Header />
+
+      <main class="flex-1 overflow-y-auto p-4 md:p-6">
+        <div class="mx-auto max-w-7xl">
+          <slot />
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
